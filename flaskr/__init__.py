ea608c0fd79c20763abd522baa29b2bfd923aca3
kahoragachau <joelkahoragachau6@gmail.com>
2023-04-11 00:48:53 +0300

    feat: Add a shoe to the database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,10 +28,45 @@ def create_app(test_config=None):
         return jsonify({
             "shoes": formatted_shoes
         })
-    # Add shoes
-    @app.route('/shoes', method=["POST"])
+    # Add a shoes
+    @app.route('/shoes', methods=["POST"])
     def add_shoe():
-        shoes = Shoe.que
+        request_data = request.get_json()
+
+        add_id = request_data["id"]
+        add_shoes_type = request_data["shoes_type"]
+        add_brand = request_data['brand']
+        add_size = request_data['size']
+        add_color = request_data['color']
+        add_prize = request_data['prize']
+        add_is_in_stock = request_data['is_in_stock']
+
+        # Set the shoe attribute
+        shoe = Shoe(id = add_id,
+                    brand = add_brand, 
+                    shoe_type = add_shoes_type, 
+                    size = add_size, 
+                    color = add_color, 
+                    prize = add_prize, 
+                    is_in_stock = add_is_in_stock)
+        
+        # Insert into database
+        shoe.insert()
+
+        return jsonify({
+            "success": True,
+            "shoes": shoe.format()
+        })
+    
+        # return jsonify({
+        #     "shoes_type": shoes_type, 
+        #     "brand" :brand, 
+        #     "size": size, 
+        #     "color": color, 
+        #     "prize":prize, 
+        #     "is_in_stock":is_in_stock
+        #     })
+
     # get a specific shoe
     @app.route('/shoes/<int:shoe_id>', methods=["GET"])
     def get_specific_shoe(shoe_id):
